5939a8e7fcb209e8f14e42be1ca9efb45b75ee8c
Jan Hassel <Jan.Hassel@ibm.com>
2026-03-24 07:39:25 +0100

    chore: add copyright banners to all files

--- docs/js/ui/controls.ts
+++ docs/js/ui/controls.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import themeIcon from '@carbon/icons/svg/32/color-palette.svg?raw';
 import modeIcon from '@carbon/icons/svg/32/scale.svg?raw';
 import sizeIcon from '@carbon/icons/svg/32/fit-to-height.svg?raw';
