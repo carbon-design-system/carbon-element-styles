3bfa387b6c6a2c878a2f4a8ba65a7f83cee96d0b
Jan Hassel <jan.hassel@ibm.com>
2026-09-25 15:18:04 +0200

    feat: add element styles for meter (#231)

---
+++ docs/content/elements/meter/content.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { CdsEsDocsElementDemoContent } from "@/components/ElementDemoContent";
+import scssDoc from "virtual:scss-docs/meter";
+import { demos } from "./_demos";
+
+const elementDemoContent = document.createElement(
+  "cds-es-docs-element-demo-content",
+) as CdsEsDocsElementDemoContent;
+
+elementDemoContent.label = "Meter";
+elementDemoContent.scssDoc = scssDoc;
+elementDemoContent.demos = demos;
+elementDemoContent.references = [
+  {
+    label: "<meter>",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/meter",
+  },
+  {
+    label: "Meter chart",
+    url: "https://charts.carbondesignsystem.com/meter",
+  },
+];
+
+export default elementDemoContent;
