3420d53832f7dd080fc77c0b27d4f43256d2d82c
Jan Hassel <Jan.Hassel@ibm.com>
2026-09-25 15:06:28 +0200

    feat: add element styles for meter

---
+++ scss/elements/meter/index.test.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright IBM Corp. 2026
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { scssExposesFunctioningPublicStylesMixin } from "../../../tests/common/scss-exposes-functioning-public-styles-mixin";
+import { usesNoDeprecatedCssFeatures } from "../../../tests/common/uses-no-deprecated-css-features";
+
+scssExposesFunctioningPublicStylesMixin({
+  module: "elements/meter",
+});
+
+usesNoDeprecatedCssFeatures({ element: "meter" });
